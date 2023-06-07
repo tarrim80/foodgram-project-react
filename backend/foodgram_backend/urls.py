04bd72bebb757a23c0d69cdbacda4e16f4793ad2
Roman Tarasenko <tarrim80@yandex.ru>
2023-06-07 12:02:46 +0600

    Update settings media

--- backend/foodgram_backend/urls.py
+++ backend/foodgram_backend/urls.py
@@ -1,8 +1,8 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-admin.site.site_header = 'Админ-панель Продуктового помощника'
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
