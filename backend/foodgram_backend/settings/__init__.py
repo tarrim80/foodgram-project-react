b483c5be454b509c5251b68c95cfcd5b1759c17d
Roman Tarasenko <tarrim80@yandex.ru>
2023-07-09 18:53:13 +0600

    Add debug-toolbar

--- backend/foodgram_backend/settings/__init__.py
+++ backend/foodgram_backend/settings/__init__.py
@@ -1,5 +1,6 @@
 from foodgram_backend.settings.base import *  # noqa
 from foodgram_backend.settings.database import *  # noqa
+from foodgram_backend.settings.debug_toolbar import *  # noqa
 from foodgram_backend.settings.djoser import *  # noqa
 from foodgram_backend.settings.drf import *  # noqa
 from foodgram_backend.settings.auth import *  # noqa
--- backend/foodgram_backend/urls.py
+++ backend/foodgram_backend/urls.py
@@ -7,3 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
