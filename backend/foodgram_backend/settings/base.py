169152eab0fb9fb6034cc1eb8490282f7e4c1bfc
Roman Tarasenko <tarrim80@yandex.ru>
2023-07-11 20:06:18 +0600

    Add docker-compose local

--- backend/foodgram_backend/settings/base.py
+++ backend/foodgram_backend/settings/base.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'secret_key_public')
 DEVELOPMENT_STATUS = os.getenv('DEVELOPMENT_STATUS', 'PRODUCTION')
 DEBUG = DEVELOPMENT_STATUS != 'PRODUCTION'
 
-ALLOWED_HOSTS = os.environ['DJANGO_ALLOWED_HOSTS'].split(',')
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '[*]')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
--- backend/foodgram_backend/settings/database.py
+++ backend/foodgram_backend/settings/database.py
@@ -1,19 +1,17 @@
 import os
 
-from foodgram_backend.settings.base import BASE_DIR
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/data/db.sqlite3',
     }
 } if os.getenv('DEVELOPMENT_STATUS') == 'DEVELOPMENT' else {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'dj'),
+        'NAME': os.getenv('POSTGRES_DB', 'db'),
         'USER': os.getenv('POSTGRES_USER', 'user'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', ''),
-        'PORT': os.getenv('DB_PORT', 5432)
+        'HOST': os.getenv('POSTGRES_HOST', ''),
+        'PORT': os.getenv('POSTGRES_PORT', 5432)
     }
 }
