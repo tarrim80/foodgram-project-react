6c42ca4513edfaaa4d3718798f36a8782f3d689f
Roman Tarasenko <tarrim80@yandex.ru>
2023-07-14 20:31:43 +0600

    Settings Database

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -94,25 +94,23 @@ PAGE_SIZE = 6
 
 # ______________DATABASES______________ #
 
-DATABASES = (
-    {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
+DATABASES = {
+    #     "default": {
+    #         "ENGINE": "django.db.backends.sqlite3",
+    #         "NAME": BASE_DIR / "db.sqlite3",
+    #     }
+    # }
+    # if os.getenv("DEVELOPMENT_STATUS") == "DEVELOPMENT"
+    # else {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB", "db"),
+        "USER": os.getenv("POSTGRES_USER", "user"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", ""),
+        "HOST": os.getenv("POSTGRES_HOST", ""),
+        "PORT": os.getenv("POSTGRES_PORT", 5432),
     }
-    if os.getenv("DEVELOPMENT_STATUS") == "DEVELOPMENT"
-    else {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.getenv("POSTGRES_DB", "db"),
-            "USER": os.getenv("POSTGRES_USER", "user"),
-            "PASSWORD": os.getenv("POSTGRES_PASSWORD", ""),
-            "HOST": os.getenv("POSTGRES_HOST", ""),
-            "PORT": os.getenv("POSTGRES_PORT", 5432),
-        }
-    }
-)
+}
 
 
 # ______________AUTH_____________ #
